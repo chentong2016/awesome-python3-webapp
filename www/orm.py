6f66d21c3ff68d0b949bbdab084d51a0fdf53d52
chentong <chentong2012@163.com>
2016-10-14 14:22:39 +0800

    until day4

--- www/orm.py
+++ www/orm.py
@@ -82,8 +82,8 @@ class ModelMetaclass(type):
             raise RuntimeError('Primary key not found')
         for k in mappings.keys():
             attrs.pop(k)
-        escaped_fields = list(map(lambda f:'`s`' % f,fields))
-        atrrs['__mappings__'] = mappings
+        escaped_fields = list(map(lambda f: '`%s`' % f,fields))
+        attrs['__mappings__'] = mappings
         attrs['__table__'] = tableName
         attrs['__primary_key__'] = primaryKey
         attrs['__fields__'] = fields
@@ -178,7 +178,7 @@ class Field(object):
         self.primary_key = primary_key
         self.default = default
     def __str__(self):
-        return '<%s,%s:%s>'% (self.__class__.name__,self.column_type,self.name)
+        return '<%s,%s:%s>' % (self.__class__.__name__,self.column_type,self.name)
 class StringField(Field):
     def __init__(self,name=None,primary_key=False,default=None,ddl='varchar(100)'):
         super().__init__(name,ddl,primary_key,default)
